7999b6924003500381e79bd8502d42fb0f4951c9
jubuyer <jahmed10@stuy.edu>
2021-09-24 21:24:40 -0400

    created checkerboard function

--- shapes.cpp
+++ shapes.cpp
@@ -14,3 +14,30 @@ std::string box(int width, int height) {
 
   return output;
 }
+
+std::string checkerboard(int width, int height) {
+  std::string output = "";
+
+  for (int h = 0; h < height; h++) { //loop for the rows to be printed
+    for (int w = 0; w < width; w++) { //loop for stars and spaces in each row
+      if ((h % 2) != 0) { //checks if even/odd row, prints either space or star
+        if ((w % 2) != 0) { //checks if even/odd column, prints space/star
+          output += "*"; //if odd row, odd column
+        } else if ((w % 2) == 0){
+          output += " "; //if odd row, even column
+        }
+      } else {
+        if ((h % 2) == 0) {
+          if ((w % 2) != 0) {
+            output += " "; //if even row, odd column
+          } else if ((w % 2) == 0) {
+            output += "*"; //if even row, even column
+          }
+        }
+      }
+    }
+    output += "\n";
+  }
+
+  return output;
+}
