1179711db86781d2c5604e8abcc5aacac830b6d0
jubuyer <jahmed10@stuy.edu>
2021-09-24 20:40:10 -0400

    Added box function

--- shapes.cpp
+++ shapes.cpp
@@ -1,1 +1,15 @@
 //Shape functions
+#include <string>
+
+std::string box(int width, int height) {
+  std::string output = "";
+
+  for (int h = height; h > 0; h--) {
+    for (int w = width; w > 0; w--) {
+      output += "*";
+    }
+    output += "\n";
+  }
+
+  return output;
+}
