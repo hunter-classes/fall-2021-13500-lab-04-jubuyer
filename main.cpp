fd2db8ac3259258ffde0c14426853726735dc7b2
jubuyer <jahmed10@stuy.edu>
2021-09-24 21:15:43 -0400

    Added tests for 4A

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //Lab 4
 #include <iostream>
+#include <string>
 #include "shapes.h"
 
 using std::cout;
@@ -7,9 +8,17 @@ using std::cin;
 using std::endl;
 
 int main () {
-  //my code
-  cout << "box\n";
-  cout << box(2,2);
-  
+  std::string seperator = "\n_________________________________________\n";
+  std::string mini_sep = "-------------------\n";
+
+  //Tests for functions from lab 4
+  //4A
+  cout << seperator;
+  cout << "                TASK A                  " << seperator;
+  cout << "Box test: 5x4\n";
+  cout << box(5,4);
+  cout << mini_sep << "Box test: 4x6\n";
+  cout << box(4,6);
+
   return 0;
 }
